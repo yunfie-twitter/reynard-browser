0207cf96f3bfe2aa532745c25a84b1f909b14403
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-04-03 00:24:34 +0000

    fix: remove --enable-linker=ld, unsupported for iOS cross-compile target

--- tools/development/build-gecko.sh
+++ tools/development/build-gecko.sh
@@ -25,7 +25,6 @@ rm -f "$FIREFOX_DIR/.mozconfig"
 	echo "ac_add_options --enable-optimize"
 	echo "ac_add_options --disable-debug"
 	echo "ac_add_options --disable-tests"
-	echo "ac_add_options --enable-linker=ld"
 } > "$FIREFOX_DIR/.mozconfig"
 
 if ! rustup target list | grep -q "^$TARGET (installed)"; then
